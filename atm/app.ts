25900cb9004429521b43f09c2a71b71a454aa5f2
Ameer Hamza Khetran <hamzaa803@gmail.com>
2022-12-13 03:09:46 +0500

    Generated a random userid and pin.

--- atm/app.ts
+++ atm/app.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+let set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+let userId = "";
+for(let i=0; i<=9; i++) {
+    let userIdgen = Math.floor(Math.random() * set.length -1) + 1;
+    userId += set.charAt(userIdgen);
+}
+
+let pin = Math.floor(Math.random() * 8999) + 1000;
+console.log(`User Id : ${userId}`);
+console.log(`PIN : ${pin}`);
+
