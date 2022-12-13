f6c885abc42a9f9caa4cf0eb38b75c7aea84077e
Ameer Hamza Khetran <hamzaa803@gmail.com>
2022-12-13 05:21:21 +0500

    added User Id and Pin verification

--- atm/app.ts
+++ atm/app.ts
@@ -1,3 +1,5 @@
+import inquirer from "inquirer";
+
 let set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 let userId = "";
@@ -10,3 +12,50 @@ let pin = Math.floor(Math.random() * 8999) + 1000;
 console.log(`User Id : ${userId}`);
 console.log(`PIN : ${pin}`);
 
+let questions = [
+    {
+        name: "userIdQuestion",
+        type: "input",
+        message: "Enter the User Id given above",
+        validate: (value:string) => {
+            if (value !== userId) {
+                console.log("\nIncorrect User Id. Please enter again correctly");
+                return false;
+            }
+            return true;
+        }
+    },
+    {
+        name: "pinQuestion",
+        type: "input",
+        message: "Enter the pin given above",
+        validate: (value:string) => {
+            if (value) {
+                let value1;
+                if (value1 = Number(value)) {
+                    if (value1 === pin) {
+                        return true;
+                    }
+                    else {
+                        console.log("\nIncorrect Pin. Please enter again correctly");
+                        return false;
+                    }
+                }
+                else {
+                    console.log("\nIncorrect Pin. Please enter again correctly");
+                    return false;
+                }
+            }
+            else {
+                console.log("\nIncorrect Pin. Please enter again correctly");
+                return false;
+            }
+        }
+    }
+]
+
+inquirer.prompt(questions)
+
+.then((answers) => {
+    console.log("Atm functionalities unlocked");
+})
