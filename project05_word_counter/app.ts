cd64ab922d288a5e2efe38f88cad1aa71efab3a2
Ameer Hamza Khetran <hamzaa803@gmail.com>
2022-12-19 08:26:28 +0500

    Added inquirer

--- project05_word_counter/app.ts
+++ project05_word_counter/app.ts
@@ -1,1 +1,31 @@
-console.log("Hello World");
+import inquirer from "inquirer";
+let question = [
+    {
+        name: "text",
+        type: "input",
+        message: "Enter something to count characters",
+        validate: ((value:string) => {
+            if (value === "") {
+                console.log("Please enter something");
+                return false;
+            }
+            return true;
+        })
+    }
+]
+
+inquirer.prompt(question)
+
+.then((answer) => {
+    console.log(answer);
+    let value = answer.text;
+    console.log(value);
+    for (const i of value) {
+        if (i === " " || i === "    ") {
+            continue;
+        }
+        else {
+            console.log(i);
+        }
+    }
+})
